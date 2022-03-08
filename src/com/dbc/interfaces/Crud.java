c9ea3caf208ffc6fbaab8e5a592992fa7cf637a1
Lucas K. P <lucaskaczyn@gmail.com>
2022-03-08 15:46:23 -0300

    novas mudanças

--- src/com/dbc/interfaces/Crud.java
+++
@@ -1,7 +0,0 @@
-package com.dbc.interfaces;
-
-public interface Crud {
-    Boolean createUser (String name, String email, String key);
-    Boolean updateUser (Integer id);
-    Boolean deleteUser ();
-}
