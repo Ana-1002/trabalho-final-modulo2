39047fa4a14bedd8f8bd0e62753ddf346ab15fa6
Lucas K. P <lucaskaczyn@gmail.com>
2022-03-09 10:12:47 -0300

    feat: verificacao

--- src/com/dbc/service/CategoryService.java
+++ src/com/dbc/service/CategoryService.java
@@ -9,24 +9,20 @@ public class CategoryService {
 
     private CategoryRepository categoryRepository;
 
-    public static void main(String[] args) {
-        //TODO - tem erro
-        CategoryService categoryService = new CategoryService();
-        categoryService.remove(11);
-
-        categoryService.list();
-    }
-
     public CategoryService() {
         categoryRepository = new CategoryRepository();
     }
 
     public void add(Category category) {
         try {
+            if (this.categoryAlreadyExists(category)) {
+                throw new Exception("ERRO: Categoria já existente");
+            }
+
             Category addedCategory = categoryRepository.add(category);
             System.out.println("Categoria adicionada com sucesso! " + addedCategory);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 
@@ -54,5 +50,14 @@ public class CategoryService {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public boolean categoryAlreadyExists(Category category) throws SQLException {
+        for (Category c : new CategoryRepository().list()) {
+            if (c.getName().toLowerCase().equalsIgnoreCase(category.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
