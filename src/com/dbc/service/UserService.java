1bbff9f05ace9f6209bfebb047258b3bd7ba596c
Nicolas Fiedler <nicolasxfiedler@gmail.com>
2022-03-09 11:09:53 -0300

    user service/repository

--- src/com/dbc/service/UserService.java
+++ src/com/dbc/service/UserService.java
@@ -1,14 +1,72 @@
-//package com.dbc.service;
-//
-//import com.dbc.entities.user.User;
-//import com.dbc.repository.UserRepository;
-//
-//public class UserService {
-//
-//    private UserRepository userRepository;
-//
-//    public User add (User user) {
-//
-//        return null;
-//    }
-//}
+package com.dbc.service;
+
+import com.dbc.entities.Category;
+import com.dbc.entities.user.User;
+import com.dbc.repository.CategoryRepository;
+import com.dbc.repository.UserRepository;
+
+import java.sql.SQLException;
+import java.util.EmptyStackException;
+import java.util.Locale;
+
+public class UserService {
+
+    //TODO - remover static
+    private static final UserRepository USER_REPOSITORY = new UserRepository();
+
+    public static void main(String[] args) throws Exception {
+        USER_REPOSITORY.list().forEach(System.out::println);
+        User u = new User("nome", "email", "senha", 1, "23456789");
+        new UserService().add(u);
+        USER_REPOSITORY.list().forEach(System.out::println);
+    }
+
+    public User add (User user) throws Exception {
+        try{
+            if (this.userAlreadyExists(user)) {
+                throw new Exception("Usuario invalido");
+            }
+
+            USER_REPOSITORY.add(user);
+            return user;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public void remove(Integer id) {
+        try {
+            boolean isRemoved = USER_REPOSITORY.remove(id);
+            System.out.println("removido? " + isRemoved + "| com id=" + id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update(Integer id, User user) {
+        try {
+            boolean isUpdated = USER_REPOSITORY.update(id, user);
+            System.out.println("editado? " + isUpdated + "| com id=" + id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void list() {
+        try {
+            USER_REPOSITORY.list().forEach(System.out::println);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean userAlreadyExists(User user) throws SQLException {
+        for (User u : new UserRepository().list()) {
+            if (u.getDocument().toLowerCase().equalsIgnoreCase(user.getDocument().toLowerCase(Locale.ROOT))) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
